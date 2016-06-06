13fad7b7f48e251db6ccbdca34af92d0e72d9220
Jean-baptiste MARSAL <jmarsal@student.42.fr>
2016-06-06 12:03:38 +0200

    Creation de la branche quadrillage
    -->	Comme je retiens de mes erreurs, je vais creer une nouvelle branche pour
    etre sur de ne pas faire de betises.

--- srcs/draw.c
+++ srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: jmarsal <jmarsal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/27 23:11:25 by jmarsal           #+#    #+#             */
-/*   Updated: 2016/06/06 11:16:28 by jmarsal          ###   ########.fr       */
+/*   Updated: 2016/06/06 12:00:55 by jmarsal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,8 @@ static void draw_lines(t_app *app)
 		test = coords;
 		x = test->x;
 		y = test->y;
+		if (test->color == 0xFFFFFF && test->z > 0)
+			coords->color = 0xa10404;
 		while (y < coords->next->y + PIX_SPACE && coords)
 		{
 			test->y = y;
