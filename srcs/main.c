aac4e6e2d40094778e283e644546a6d2936e580e
jmarsal <jmarsal@student.42.fr>
2016-06-16 22:14:03 +0200

    push

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jmarsal <jmarsal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/09 15:49:52 by jmarsal           #+#    #+#             */
-/*   Updated: 2016/06/16 15:40:25 by jmarsal          ###   ########.fr       */
+/*   Updated: 2016/06/16 22:12:36 by jmarsal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,44 +24,43 @@ static int		error_read(t_error err, const char *av, int fd)
 	return (0);
 }
 
-static int		get_data_in_line(t_app *app, t_data *data, char *line,
-									t_coords *c_data)
-{
-	if (data->helper.line % (data->oldsize - 1) == 0)
-	{
-		if (!(data->data_elem = ft_realloc(data->data_elem,
-			data->newsize * sizeof(t_coords), data->oldsize)))
-			return (-1);
-		data->oldsize = data->newsize;
-		data->newsize = data->oldsize * 2;
-	}
-	if ((get_data(app, line, c_data, data)) == -1)
-		return (print_error(app->err, 2));
-	c_data->y += app->win->space_pix;
-	app->params->y_max++;
-	data->helper.line++;
-	return (0);
-}
-
 static int		read_file(const char **av, t_app *app)
 {
 	t_coords	*c_data;
+	size_t		i;
 	char		*line;
 	int			fd;
 
+	app->data->is_colors = 0;
+	app->data->helper.line = 0;
+	i = 0;
 	read_name_for_size_win(av[1], app->win);
 	if ((c_data = init_coords(0, 0, 0, 0)) == NULL)
 		return (-1);
 	fd = open(av[1], O_RDONLY);
 	if (error_read(app->err, av[1], fd) == -1)
 		return (-1);
+	while ((ft_get_next_line(fd, &line)) > 0)
+	{
+		app->params->x_max = ft_strsplit(line, ' ');
+		app->params->y_max++;
+	}
+	t_coords tab[app->params->y_max][app->params->x_max];
+	app->data->helper.line = app->params->y_max;
+	if (app->params->y_max == 0)
+		return (print_error(app->err, 1));
+	if (close(fd) == -1)
+		return (-1);
 	fd = open(av[1], O_RDONLY);
 	while ((ft_get_next_line(fd, &line)) > 0)
-		if ((get_data_in_line(app, app->data, line, c_data)) == -1)
-			return (-1);
+	{
+		app->params->x_max = 0;
+		if ((get_data(app, line, c_data, app->data, tab)) == -1)
+			return (print_error(app->err, 2));
+		c_data->y += app->win->space_pix;
+		app->data->helper.line++;
+	}
 	free (c_data);
-	if (app->data->helper.line == 0)
-		return (print_error(app->err, 1));
 	if (close(fd) == -1)
 		return (-1);
 	return (0);
@@ -80,7 +79,7 @@ int		main(int ac, char **av)
 		}
 		if (read_file((const char**)av, app) == -1)
 			exit (-1);
-		mlx_start(app);
+		// mlx_start(app);
 	}
 	else
 		ft_putstr("\n\033[31mERROR\033[0m\n--> usage : ./fdf template.fdf\n");
