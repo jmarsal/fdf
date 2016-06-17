425391d441cd9b111906a929cfb96183b6557a42
jmarsal <jmarsal@student.42.fr>
2016-06-17 23:39:56 +0200

    Lignes Ok
    --> Les lignes sont tracées correctement
    --> Place aux verticales
    
    --> Encore des segfaults...

--- srcs/draw.c
+++ srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: jmarsal <jmarsal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/27 23:11:25 by jmarsal           #+#    #+#             */
-/*   Updated: 2016/06/17 16:17:58 by jmarsal          ###   ########.fr       */
+/*   Updated: 2016/06/17 23:37:25 by jmarsal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,68 +43,76 @@ void	mlx_put_pixel_to_image(t_app *app, t_coords *c, int color)
 		y++;
 	}
 }*/
-void	draw_line(t_app *app, t_coords *c_data, t_coords *n_data)
+void	draw_line(t_app *app, t_coords **n_data, size_t lines, size_t elems)
 {
-	t_coords	*tmp;
-	int			y;
-	int			x;
-	int			color;
-	tmp = c_data;
-	y = c_data->y;
-	x = c_data->x;
-	color = c_data->color;
-	while (x < n_data->x)
+	t_coords	c_elems;
+	t_coords	n_elems;
+	t_coords	n_line;
+
+	c_elems = n_data[lines][elems];
+	n_elems = n_data[lines][elems + 1];
+	n_line = n_data[lines + 1][elems];
+	while (c_elems.x < n_elems.x)
 	{
-		tmp->x = x;
-		if (y < n_data->y)
-			y++;
-		else if (y > n_data->y)
-			y--;
-		tmp->y = y;
-		if ((y != n_data->y && c_data->z == 0) ||
-		(y != n_data->y && n_data->z == 0))
-			tmp->color = 0xFFFFFF;
-		else if (n_data->z > 0)
+		if (c_elems.y < n_elems.y)
+			c_elems.y++;
+		else if (c_elems.y > n_elems.y)
+			c_elems.y--;
+		if (c_elems.y != n_elems.y && c_elems.z == 0)
+			c_elems.color = 0xFFFFFF;
+		else if (n_elems.z > 0 || (n_elems.z != n_elems.z && n_elems.z > 0))
 		{
-			n_data->color = 0xff0000;
-			tmp->color = n_data->color;
+			n_elems.color = 0xff0000;
+			c_elems.color = n_elems.color;
 		}
-		else if (c_data->z == 0)
-			tmp->color = 0xFFFFFF;
-		mlx_put_pixel_to_image(app, tmp, tmp->color);
-		x++;
+		else if (n_elems.z == 0)
+			c_elems.color = 0xFFFFFF;
+		mlx_put_pixel_to_image(app, &c_elems, c_elems.color);
+		c_elems.x++;
 	}
 }
 
-static t_coords *new_data(t_win *win, t_coords *c_data, t_data *data)
+static t_coords **new_data(t_data *data, t_params *params, t_win *win,
+							t_coords **n_data)
 {
-	c_data->x += (((c_data->x * win->zoom) + c_data->z) + win->move);
-	c_data->y += (((c_data->y * win->zoom) - c_data->z) + win->move);
-	c_data->color = (c_data->z > 0 && data->is_colors == 0) ?
-					0xff0000 : c_data->color;
-	return (c_data);
+	size_t		lines;
+	size_t		elems;
+
+	lines = 0;
+	while (lines < params->y_max)
+	{
+		elems = 0;
+		while (elems < params->x_max)
+		{
+			n_data[lines][elems].x += (((n_data[lines][elems].x * win->zoom) +
+										n_data[lines][elems].z) + win->move);
+			n_data[lines][elems].y += (((n_data[lines][elems].y * win->zoom) -
+										n_data[lines][elems].z) + win->move);
+			n_data[lines][elems].color = (n_data[lines][elems].z > 0 &&
+				data->is_colors == 0) ? 0xff0000 : n_data[lines][elems].color;
+			++elems;
+		}
+		++lines;
+	}
+	return (n_data);
 }
 
 void		draw_windows(t_app *app)
 {
+	t_coords	**n_data;
 	size_t		lines;
 	size_t		elems;
-	t_coords	*c_data;
-	t_coords	*n_data;
 
+	n_data = new_data(app->data, app->params, app->win, app->data->data_elem);
 	lines = 0;
 	while (lines < app->params->y_max)
 	{
 		elems = 0;
 		while (elems < app->params->x_max)
 		{
-			c_data = new_data(app->win, &app->data->data_elem[lines][elems],
-								app->data);
-			if (elems + 1 < app->params->x_max)
-				n_data = new_data(app->win,
-						&app->data->data_elem[lines][elems + 1], app->data);
-			// mlx_put_pixel_to_image(app, c_data, c_data->color);
-			draw_line(app, c_data, n_data);
+			// mlx_put_pixel_to_image(app, &n_data[lines][elems],
+									// n_data[lines][elems].color);
+			draw_line(app, n_data, lines, elems);
 			// draw_columns(app, coords_cur, coords_next, i);
 			++elems;
 		}
